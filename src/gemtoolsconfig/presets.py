a9443278086e925b25f2566b8a2269800c62c299
rlioret <robin.lioret@capgemini.com>
2023-04-01 13:10:35 +0200

    add docstrings

--- src/gemtoolsconfig/presets.py
+++ src/gemtoolsconfig/presets.py
@@ -7,6 +7,12 @@ from .handlers import from_source, DEFAULT_PATH, get_file_handler, get_find_suit
 
 
 def source_loader() -> ConfigurationLoader:
+    """
+    Get a configuration loader that loads configuration data from a source string.
+
+    :return: A ConfigurationLoader instance that can be used to load configuration data from a source string.
+    :rtype: ConfigurationLoader
+    """
     builder = ConfigurationLoaderBuilder()
     builder.add_loading_handler(from_source)
     return builder.build()
@@ -15,6 +21,18 @@ def source_loader() -> ConfigurationLoader:
 def file_loader(directory: Union[PathLike, str] = DEFAULT_PATH,
                 key_file: Union[PathLike, str] = None
                 ) -> ConfigurationLoader:
+    """
+    Get a configuration loader that loads configuration data from a file.
+
+    :param directory: The directory where the configuration file is located. Defaults to the current directory.
+    :type directory: Union[PathLike, str]
+    :param key_file: Optional path to the file containing the encryption key for encrypted configuration files.
+                     Defaults to None.
+    :type key_file: Union[PathLike, str], optional
+    :return: A ConfigurationLoader instance that can be used to load configuration data from a file.
+    :rtype: ConfigurationLoader
+    :raises: NotADirectoryError if the specified directory does not exist.
+    """
     key = None
     if key_file is not None:
         key = Path(key_file).read_bytes()
