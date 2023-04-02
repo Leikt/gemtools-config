f930575d106414de6a2a688e4fd90b9d44619860
rlioret <robin.lioret@capgemini.com>
2023-04-02 03:33:43 +0200

    Add docstring to quick_setup

--- src/gemtoolsconfig/__init__.py
+++ src/gemtoolsconfig/__init__.py
@@ -6,7 +6,19 @@ from .loader import ConfigurationLoader, LoadingHandler, LazyHandler, Configurat
 from .presets import preset_source_loader, preset_file_loader
 
 
-def quick_setup(directory: str = '.') -> ConfigurationItem:
-    Configurations.add_loader(preset_file_loader(directory))
+def quick_setup(directory: str = None) -> ConfigurationItem:
+    """
+    Initialize Configurations in the most standard way: a "config" file (toml, ini, yml, json, yaml) in the given
+    directory. It adds a file loader and load the default configuration.
+
+    :param directory: The directory where to find the configuration files. By default, it's the working directory.
+    :type directory: str
+    :return: the loaded configuration
+    :rtype: ConfigurationItem
+    """
+    if directory is None:
+        Configurations.add_loader(preset_file_loader())
+    else:
+        Configurations.add_loader(preset_file_loader(directory))
     Configurations.get_loader().load()
     return Configurations.get_config()
